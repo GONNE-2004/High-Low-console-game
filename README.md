287485b7549fad8b6338108c4a4a70c6696b51a9
GONNE <gonne411@gmail.com>
2025-08-09 07:04:01 +0200

    Just added it, the project implements core python basics

---
+++ README.md
@@ -0,0 +1,21 @@
+TITLE : HIGH LOW COMPARISON GAME
+
+-DESCRIPTION:
+=>The project implements the core concepts of python to a program that compares the follower count of famous personalities
+
+FEATURES:
+-Use random module to iterate through the data list in "game_data.py"
+-Us os module to professional clear screen after every guess
+-All logic are separated in functions for clear readibility
+-Implements recurssion to restart the game, use conditional for comparisons,
+-Has doc-strings to explain a function  use, comments to exxplain game logic
+
+TOOLS:
+-Python 3.x
+-No external libraries required (uses `random`, 'os' and standard modules).
+
+CODE STRUCTURE:
+-'main.py' : all the game logic
+-'art.py' : all the ACSII art for vs and hgihlow
+-'game_data.py' : contains lists of the personlities to be compared
+-'pseudocode.txt' : contains simple english explanation of the program
---
+++ main.py
@@ -0,0 +1,89 @@
+from art import logo,vs
+from game_data import data
+import random
+import os
+
+# first import the top logo and print it
+#2nd import the game data
+print(logo)
+
+def clear_screen():
+  """Clears the terminal when for another guess"""
+  # os.system checks whether the os is windows,mac or linux then uses the right command to clear terminal screen
+  try:
+    os.system("cls" if os.name == "nt" else "clear")
+  except:
+      print("\n" * 25) #Just in case the upper doesn't work in some IDLE
+
+
+
+#Write a function the compares the follower count
+def compare_follower(user_guess, a_follower, b_follower):
+    """Holds comparsion between follower count"""
+    if a_follower > b_follower:
+        return user_guess == "a"
+    else:
+        return  user_guess == "b"
+
+
+#write a function that returns the attributes
+def attributes(person):
+    """Returns the formated user charactersistics"""
+    return f"{person["name"]}, {person["description"]}, from {person["country"]}"
+
+
+
+def main():
+  """prints the logos,keeps the game in loop, and game restart"""
+  score = 0
+  #generate a random person from data
+  person_b = random.choice(data)
+  play_game = True
+
+  while play_game:
+      #making person B become the next person A
+      person_a = person_b
+      person_b = random.choice(data)
+
+      #making sure person A isn't same like person B
+      while person_a == person_b:
+          person_b = random.choice(data)
+
+      print(f"Compare A :{ attributes(person_a)}")
+      print(vs)
+      print(f"Against B : {attributes(person_b)}")
+
+      #asking th user who has more follower again and  again
+      guess = input("Who has more followers? Type 'a' or 'b':  ").lower().strip()
+      while guess not in ['a','b']:
+              guess = input("Who has more followers? Type 'a' or 'b':  ").lower().strip()
+
+      #clear screen after each guess
+      clear_screen()
+      print(logo)
+
+      #getting the follower count
+      a_follower_count = person_a["follower_count"]
+      b_follower_count = person_b["follower_count"]
+
+      #checking user guess
+      is_correct = compare_follower(guess, a_follower_count,b_follower_count)
+
+      #keeping user score and giving feedback
+      if is_correct:
+          score += 1
+          print(f"You are right! Current score: {score}")
+      else:
+          print(f"You are wrong! Final score: {score}")
+          play_again = input("Play again (y/n):  ").lower().strip()
+          if play_again == "y":
+            main() # restarts game in case user failed, it's implemented using recurssion
+          else:
+            play_game = False # ends game
+
+
+
+
+
+
+main()
